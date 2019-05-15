afbe9aa588b4ee379bd68ee5e435c751299e826f
Tank <tank.bai@qq.com>
2019-05-15 16:56:45 +0800

    use the center of CD as evaluation points

--- Python/env_error_check.py
+++ Python/env_error_check.py
@@ -2,8 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-park_width_2 = 6.1 / 2.0
-park_depth_2 = 1.9 / 2.0
+ref_park_width_2 = 6.0 / 2.0
+ref_park_depth = 2.2
 
 car_wheel_base = 2.67
 car_width_2 = 1.77 / 2.0
@@ -50,24 +50,24 @@ def get_car_point(x, y, yaw):
     return car_x, car_y
 
 def get_park_point(x, y, yaw):
-    global park_width_2
-    global park_depth_2
+    global ref_park_width_2
+    global ref_park_depth
     cos_yaw = np.cos(yaw)
     sin_yaw = np.sin(yaw)
 
     park_x = []
     park_y = []
-    park_x.append(-park_width_2 * cos_yaw - park_depth_2 * sin_yaw + x)
-    park_x.append(-park_width_2 * cos_yaw + park_depth_2 * sin_yaw + x)
-    park_x.append(park_width_2 * cos_yaw + park_depth_2 * sin_yaw + x)
-    park_x.append(park_width_2 * cos_yaw - park_depth_2 * sin_yaw + x)
-    park_x.append(-park_width_2 * cos_yaw - park_depth_2 * sin_yaw + x)
+    park_x.append(-ref_park_width_2 * cos_yaw + x)
+    park_x.append(-ref_park_width_2 * cos_yaw + ref_park_depth * sin_yaw + x)
+    park_x.append(ref_park_width_2 * cos_yaw + ref_park_depth * sin_yaw + x)
+    park_x.append(ref_park_width_2 * cos_yaw + x)
+    park_x.append(-ref_park_width_2 * cos_yaw + x)
 
-    park_y.append(-park_width_2 * sin_yaw + park_depth_2 * cos_yaw + y)
-    park_y.append(-park_width_2 * sin_yaw - park_depth_2 * cos_yaw + y)
-    park_y.append(park_width_2 * sin_yaw - park_depth_2 * cos_yaw + y)
-    park_y.append(park_width_2 * sin_yaw + park_depth_2 * cos_yaw + y)
-    park_y.append(-park_width_2 * sin_yaw + park_depth_2 * cos_yaw + y)
+    park_y.append(-ref_park_width_2 * sin_yaw + y)
+    park_y.append(-ref_park_width_2 * sin_yaw - ref_park_depth * cos_yaw + y)
+    park_y.append(ref_park_width_2 * sin_yaw - ref_park_depth * cos_yaw + y)
+    park_y.append(ref_park_width_2 * sin_yaw + y)
+    park_y.append(-ref_park_width_2 * sin_yaw + y)
 
     return park_x, park_y
 
@@ -82,8 +82,8 @@ def draw_all():
     global park_y_mea
     global park_yaw_mea
     car_x_gt_list, car_y_gt_list = get_car_point(car_x_gt, car_y_gt, car_yaw_gt)
-    park_x_gt_list, park_y_gt_list = get_car_point(park_x_gt, park_y_gt, park_yaw_gt)
-    park_x_mea_list, park_y_mea_list = get_car_point(park_x_mea, park_y_mea, park_yaw_mea)
+    park_x_gt_list, park_y_gt_list = get_park_point(park_x_gt, park_y_gt, park_yaw_gt)
+    park_x_mea_list, park_y_mea_list = get_park_point(park_x_mea, park_y_mea, park_yaw_mea)
     plt.plot(car_x_gt_list, car_y_gt_list, 'b-')
     plt.plot(park_x_gt_list, park_y_gt_list, 'r-')
     plt.plot(park_x_mea_list, park_y_mea_list, 'r--')
@@ -91,10 +91,14 @@ def draw_all():
     plt.show()
 
 
-rear_wheel_x = float(input("请输入右后车轮在车位坐标系下的测量值x:"))
-rear_wheel_y = float(input("请输入右后车轮在车位坐标系下的测量值y:"))
-front_wheel_x = float(input("请输入右前车轮在车位坐标系下的测量值x:"))
-front_wheel_y = float(input("请输入右前车轮在车位坐标系下的测量值y:"))
+park_x_gt = ref_park_width_2
+park_y_gt = 0.0
+park_yaw_gt = 0.0
+
+rear_wheel_x = float(input("请输入后车轮在车位坐标系下的测量值x:"))
+rear_wheel_y = float(input("请输入后车轮在车位坐标系下的测量值y:"))
+front_wheel_x = float(input("请输入前车轮在车位坐标系下的测量值x:"))
+front_wheel_y = float(input("请输入前车轮在车位坐标系下的测量值y:"))
 car_yaw_gt = np.arctan2(front_wheel_y - rear_wheel_y, front_wheel_x - rear_wheel_x)
 car_x_gt = rear_wheel_x + car_width_2 * np.cos(car_yaw_gt + math.pi / 2)
 car_y_gt = rear_wheel_y + car_width_2 * np.sin(car_yaw_gt + math.pi / 2)
@@ -111,19 +115,28 @@ point_c_x_w = float(input("请输入车位C点在世界坐标系下的记录值x
 point_c_y_w = float(input("请输入车位C点在世界坐标系下的记录值y:"))
 point_d_x_w = float(input("请输入车位D点在世界坐标系下的记录值x:"))
 point_d_y_w = float(input("请输入车位D点在世界坐标系下的记录值y:"))
-x_w_p = point_d_x_w
-y_w_p = point_d_y_w
-theta_w_p = np.arctan2(point_c_y_w - point_d_y_w, point_c_x_w - point_d_x_w)
-cos_theta_w_p = np.cos(theta_w_p)
-sin_theta_w_p = np.sin(theta_w_p)
 
-car_yaw_mea = car_yaw_mea_w - theta_w_p
-car_x_mea = cos_theta_w_p * (car_x_mea_w - x_w_p) + sin_theta_w_p * (car_y_mea_w - y_w_p)
-car_y_mea = -sin_theta_w_p * (car_x_mea_w - x_w_p) + cos_theta_w_p * (car_y_mea_w - y_w_p)
+park_x_mea_w = (point_c_x_w + point_d_x_w) / 2.0
+park_y_mea_w = (point_c_y_w + point_d_y_w) / 2.0
+park_theta_mea_w = np.arctan2(point_c_y_w - point_d_y_w, point_c_x_w - point_d_x_w)
 
-mea_error = np.sqrt((front_wheel_y - rear_wheel_y)**2 + (front_wheel_x - rear_wheel_x)**2) / car_wheel_base
+theta_w_c = car_yaw_mea_w
+cos_theta_w_c = np.cos(theta_w_c)
+sin_theta_w_c = np.sin(theta_w_c)
+park_theta_mea_c = park_theta_mea_w - theta_w_c
+park_x_mea_c = cos_theta_w_c * (park_x_mea_w - car_x_mea_w) + sin_theta_w_c * (park_y_mea_w - car_y_mea_w)
+park_y_mea_c = -sin_theta_w_c * (park_x_mea_w - car_x_mea_w) + cos_theta_w_c * (park_y_mea_w - car_y_mea_w)
+
+theta_p_c = car_yaw_gt - park_yaw_gt
+cos_theta_p_c = np.cos(theta_p_c)
+sin_theta_p_c = np.sin(theta_p_c)
+park_yaw_mea = park_theta_mea_c - theta_p_c
+park_x_mea = cos_theta_p_c * park_x_mea_c - sin_theta_p_c * park_y_mea_c + car_x_gt
+park_y_mea = sin_theta_p_c * park_x_mea_c + cos_theta_p_c * park_y_mea_c + car_y_gt
+
+mea_error = 1 - np.sqrt((front_wheel_y - rear_wheel_y)**2 + (front_wheel_x - rear_wheel_x)**2) / car_wheel_base
 print("测量误差:", mea_error)
-print("车辆x方向误差:", car_x_mea - car_x_gt)
-print("车辆y方向误差:", car_y_mea - car_y_gt)
-print("车辆yaw方向误差:", math.degrees(car_yaw_mea - car_yaw_gt))
+print("车辆x方向误差:", park_x_mea - park_x_gt)
+print("车辆y方向误差:", park_y_mea - park_y_gt)
+print("车辆yaw方向误差:", math.degrees(park_yaw_mea - park_yaw_gt))
 draw_all()
